307f233a92abc810575779560fd560348e861694
D-Johnson89 <d.l.johnson3009@gmail.com>
2025-10-02 15:00:39 -0400

    update: add first js lesson to repo

---
+++ javascriptCourse/sentenceMaker.js
@@ -0,0 +1,30 @@
+/*  ===================================
+    INITIAL VARIABLES
+    ===================================  */
+
+let adjective = "angry";
+let noun = "troll";
+let verb = "yelling";
+let place = "tunnel";
+let adjective2 ="flared";
+let noun2 = "plums";
+
+/*  ===================================
+    STORY TIME VARIABLES
+    ===================================  */
+
+let firstStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`
+
+console.log(`First story: ${firstStory}`);
+
+adjective = "evil";
+noun = "wizard";
+verb = "standing";
+place = "dark tower"
+adjective2 = "menacing";
+noun2 = "power";
+
+/* Technically my second story didnt pass as is, they wanted the same layout using different variable values */
+let secondStory = `In the distant future, there will be a(n) ${adjective} ${noun} who will be hungry for ${noun2}. The ${noun} lives at ${place} and has a ${adjective2} presence that gives all the chills just by ${verb} there.`
+
+console.log(`Second story: ${secondStory}`);
