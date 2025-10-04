fbaea6d73aaea0201ab7297304bd5b6625f7c285
D-Johnson89 <d.l.johnson3009@gmail.com>
2025-10-04 12:53:34 -0400

    update: add earlier js labs to github trivia bot being first js lab in course

---
+++ javascriptCourse/triviaBot.js
@@ -0,0 +1,33 @@
+// Greeting Log
+
+console.log("Hello! I'm your coding fun fact guide!");
+
+/*  ===================================
+    INITIAL VARIABLES
+    ===================================  */
+
+let botName = "Teacher Bot";
+let botLocation = "the Metaverse";
+let favoriteLanguage = "JavaScript";
+
+// Bot Greeting
+
+console.log(`My name is ${botName} and I live on ${botLocation}.`);
+console.log(`My favorite programming language is ${favoriteLanguage}.`);
+
+/*  ===================================
+    CODING FACTS
+    ===================================  */
+
+let codingFact = `${favoriteLanguage} is zero-indexed, meaning items in arrays and such start at 0 not 1.`;
+console.log(codingFact);
+
+codingFact = `Semi-colons are used to end statements in ${favoriteLanguage}.`;
+console.log(codingFact);
+
+codingFact = `In ${favoriteLanguage}, let and const are used to declare variables.`;
+console.log(codingFact);
+
+// Bot Farewell
+
+console.log(`It was fun sharing these facts with you. Goodbye! - ${botName} from ${botLocation}.`);
